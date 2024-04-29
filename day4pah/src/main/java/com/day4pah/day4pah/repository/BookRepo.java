107fef832f25f4728bd5225eb7a476846714b2f3
Vishal-lahsiv <121651397+Vishal-lahsiv@users.noreply.github.com>
2024-04-29 14:52:11 +0530

    Delete day4pah directory

--- day4pah/src/main/java/com/day4pah/day4pah/repository/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.day4pah.day4pah.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.day4pah.day4pah.model.Book;
-
-public interface BookRepo extends JpaRepository<Book,Integer> {
-
-}
