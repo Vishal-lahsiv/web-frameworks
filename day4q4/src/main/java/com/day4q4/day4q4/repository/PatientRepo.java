3714bd5a3bdeb071c10775507ec035808638be46
Vishal-lahsiv <121651397+Vishal-lahsiv@users.noreply.github.com>
2024-04-29 14:52:51 +0530

    Delete day4q4 directory

--- day4q4/src/main/java/com/day4q4/day4q4/repository/PatientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.day4q4.day4q4.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.day4q4.day4q4.model.Patient;
-
-public interface PatientRepo extends JpaRepository<Patient,Integer> {
-
-}
